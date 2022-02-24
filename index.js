26beef9df87dd91b5cc20ff67cadc7bc6cb2a2f7
Will <willchesson@gmail.com>
2022-02-24 13:17:41 -0500

    commit tree 8ed902bf2c3beffd432e632377e020974fba25a1

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
+
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  for (let i = 0; i < array.length; i++) {
+    const complement = (target - array[i])
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] === complement) {
+        return true
+      }
+    }
+  }
+  return false
 }
+
+
+
+// hasTargetSum([1, 3, 5, 20], 25);
 
 /* 
   Write the Big O time complexity of your function here
@@ -8,10 +21,16 @@ function hasTargetSum(array, target) {
 
 /* 
   Add your pseudocode here
+create a nested iteration like the comparing socks functions
+if [i] + [j] = target integer, return true, else false
+
 */
 
 /*
   Add written explanation of your solution here
+  write a function that takes two arguments: an array of integer and a target integer
+  the function should iterate over the array and return true if any 2 integers 
+    add up to the target integer
 */
 
 // You can run `node index.js` to view these console logs
